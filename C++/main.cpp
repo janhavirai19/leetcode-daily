786dde937ab9e94a47152084e9e19e73be81aebb
janhavirai19 <janhavirai932@gmail.com>
2026-08-07 02:25:04 +0530

    Add LeetCode solutions

---
+++ C++/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+int age=19;
+float height = 5.7;
+double balance = 1234.78994848;
+char grade ='A';
+string city="thane";
+bool placed = false;
+cout<<age<<endl;
+cout<<height<<endl;
+cout<<balance<<endl;
+cout<<grade<<endl;
+cout<<city<<endl;
+cout<<placed<<endl;
+
+}
