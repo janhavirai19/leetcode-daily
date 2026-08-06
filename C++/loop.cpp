60712b5a4af528104b8b2f204b779478b8ea34ec
janhavirai19 <janhavirai932@gmail.com>
2026-08-07 03:09:31 +0530

    Add LeetCode solutions

--- C++/loop.cpp
+++ C++/loop.cpp
@@ -1,4 +1,5 @@
-//for loop
+// FOR LOOP>>>>>>>>>>>>>>
+
 // #include<iostream>
 // using namespace std;
 // int main(){
@@ -9,10 +10,19 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=10;i++){
+//         cout<<"hello world"<<endl;
+//     }
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1;i<=10;i++){
-        cout<<"hello world"<<endl;
+    for(int i=2;i<=10;i=i+2){
+        cout<<i<<" ";
     }
 }
