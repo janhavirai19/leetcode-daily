75a02f73bb741dd6eb1add4ed45912916915702a
janhavirai19 <janhavirai932@gmail.com>
2026-08-08 02:48:41 +0530

    Add LeetCode solutions

--- C++/Function.cpp
+++ C++/Function.cpp
@@ -1,15 +1,30 @@
 //   FUNCTION ;->>>>>
 
-#include<iostream>
-using namespace std;
-void greet(){
-    cout<<"HELLO WORLD"<<endl;
-}
-void janu(string name){
-        cout<<"HELLO"<<name<<endl;
+// #include<iostream>
+// using namespace std;
+// void greet(){
+//     cout<<"HELLO WORLD"<<endl;
+// }
+// void janu(string name){
+//         cout<<"HELLO"<<name<<endl;
 
-}
-int main(){
-    greet();
-    janu("JANHAVI");
-}
+// }
+// int main(){
+//     greet();
+//     janu(" JANHAVI");
+// }
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// void add(int a,int b){
+//     cout<<a+b;
+// }
+// int main()
+// {
+//     add(255,45);
+// }
